b159580a51e02a03961c2c36b6b6189225f53937
Etienne <etiennewazer@gmail.com>
2023-05-07 21:35:24 +0200

    Ajout des tests et des modifications de la bank

--- php/src/MoneyProblem/Domain/Bank.php
+++ php/src/MoneyProblem/Domain/Bank.php
@@ -7,25 +7,59 @@ use function array_key_exists;
 
 class Bank
 {
-    private array $exchangeRates = [];
+    private array $exchangeRates;
+    private Currency $pivotCurrency;
 
-    private function __construct(array $exchangeRates = [])
+    private function __construct(array $exchangeRates, Currency $pivotCurrency)
     {
         $this->exchangeRates = $exchangeRates;
+        $this->pivotCurrency = $pivotCurrency;
     }
 
-    public static function create(Currency $from, Currency $to, float $rate): Bank
+    public static function create(Currency $from, Currency $to, float $rate, Currency $pivotCurrency): Bank
     {
-        $bank = new Bank([]);
-        return $bank->addEchangeRate($from, $to, $rate);
+        $bank = new Bank([], $pivotCurrency);
+        return $bank->addExchangeRate($from, $to, $rate);
     }
 
-    public function addEchangeRate(Currency $from, Currency $to, float $changeRate): Bank
+    public function addExchangeRate(Currency $from, Currency $to, float $changeRate): Bank
     {
+        if (array_key_exists($from . '->' . $to, $this->exchangeRates)) {
+            if($this->exchangeRates[($from . '->' . $to)] === $changeRate){
+                throw new \InvalidArgumentException('An Exchange Rate can not be added for the Pivot Currency');
+            }
+        }
+        if ($changeRate <= 0) {
+            throw new \InvalidArgumentException('Exchange Rate should be greater than 0');
+        }
+
         $exchangeRates = array_merge($this->exchangeRates, [($from . '->' . $to) => $changeRate]);
+        $exchangeRates = array_merge($exchangeRates, [($to . '->' . $from) => 1/$changeRate]);
 
-        return new Bank($exchangeRates);
+        return new Bank($exchangeRates, $this->pivotCurrency);
     }
+
+//    public function convert(Money $money, Currency $to): Money
+//    {
+//        if(array_key_exists($this->pivotCurrency . '->' . $money->getCurrency(), $this->exchangeRates)
+//            && array_key_exists($this->pivotCurrency . '->' . $to, $this->exchangeRates)){
+//            $exchangeRateFrom = $this->exchangeRates[($this->pivotCurrency . '->' . $money->getCurrency())];
+//            $exchangeRateTo = $this->exchangeRates[($this->pivotCurrency . '->' . $to)];
+//            $exchangeRate = $exchangeRateFrom * $exchangeRateTo;
+//            (new Money($money->getAmount() * $exchangeRate, $to));
+//        }
+//        else {
+//            if (!$this->canConvert($money->getCurrency(), $to)) {
+//                throw new MissingExchangeRateException($money->getCurrency(), $to);
+//            }
+//        }
+//
+//        return $money->getCurrency() == $to
+//            ? $money
+//            : (new Money($money->getAmount() * $this->exchangeRates[($money->getCurrency() . '->' . $to)], $to));
+//    }
+
+
 
     public function convert(Money $money, Currency $to): Money
     {
@@ -38,8 +72,52 @@ class Bank
             : (new Money($money->getAmount() * $this->exchangeRates[($money->getCurrency() . '->' . $to)], $to));
     }
 
+
+
+
     private function canConvert(Currency $from, Currency $to): bool
     {
-        return $from == $to || array_key_exists($from . '->' . $to, $this->exchangeRates);
+        if ($from == $to || array_key_exists($from . '->' . $to, $this->exchangeRates)) {
+            return true;
+        }
+//        if(array_key_exists($this->pivotCurrency->getValue() . '->' . $from->getValue(), $this->exchangeRates)
+//            && array_key_exists(Currency::EUR()->getValue() . '->' . $to->getValue(), $this->exchangeRates)){
+//
+//            // KRW -> USD = (Taux Pivot/KRW) x (Taux Pivot/USD)
+//            $changeRateFrom = $this->exchangeRates[(Currency::EUR()->getValue() . '->' . $from->getValue())];
+//            $changeRateTo = $this->exchangeRates[(Currency::EUR()->getValue() . '->' . $to->getValue())];
+//            $changeRate = $changeRateFrom * $changeRateTo;
+//            $this->addExchangeRate($from, $to, $changeRate);
+//            return true;
+//        }
+        return false;
     }
+
+    public function getPivotCurrency(): Currency
+    {
+        return $this->pivotCurrency;
+    }
+    public function setPivotCurrency(Currency $pivotCurrency): void
+    {
+        $this->pivotCurrency = $pivotCurrency;
+    }
+
+    public function getExchangeRates(): array
+    {
+        return $this->exchangeRates;
+    }
+
+    /*private function updateExchangeRates(): void
+    {
+        if ($this->pivotCurrency !== null) {
+            foreach ($this->exchangeRates as $key => $exchangeRate) {
+                [$from, $to] = explode('->', $key);
+                if ($from !== $this->pivotCurrency->getValue() && $to !== $this->pivotCurrency->getValue()) {
+                    $fromToPivot = $this->exchangeRates[$from . '->' . $this->pivotCurrency->getValue()];
+                    $pivotToTo = $this->exchangeRates[$this->pivotCurrency->getValue() . '->' . $to];
+                    $this->exchangeRates[$key] = $exchangeRate / $fromToPivot * $pivotToTo;
+                }
+            }
+        }
+    }*/
 }
--- php/tests/MoneyProblem/BankTest.php
+++ php/tests/MoneyProblem/BankTest.php
@@ -59,10 +59,100 @@ class BankTest extends TestCase
         $this->assertEquals(new Money(12, Currency::USD()), $convertedAmount);
 
         // Act
-        $convertedAmount = $this->bank->addEchangeRate(Currency::EUR(), Currency::USD(), 1.3)
+        $convertedAmount = $this->bank->addExchangeRate(Currency::EUR(), Currency::USD(), 1.3)
             ->convert(new Money(10, Currency::EUR()), Currency::USD());
 
         // Assert
         $this->assertEquals(new Money(13, Currency::USD()), $convertedAmount);
     }
+
+    //Define Pivot Currency
+    public function test_set_eur_as_pivot()
+    {
+        // Arrange
+        $bank2 = BankBuilder::aBank()
+            ->withPivotCurrency(Currency::EUR())
+            ->withExchangeRate(1.2, Currency::USD())
+            ->build();
+
+        // Assert
+        $this->assertEquals(Currency::EUR(), $bank2->getPivotCurrency());
+    }
+
+
+    //Add an exchange rate
+    /*public function test_convert_krw_to_usd_with_eur_as_pivot()
+    {
+        // Arrange
+        $bank2 = BankBuilder::aBank()
+            ->withPivotCurrency(Currency::EUR())
+            ->withExchangeRate(1.2, Currency::USD())
+            ->withExchangeRate(1344, Currency::KRW())
+            ->build();
+
+        // Act
+        $krwAmount = new Money(10000, Currency::KRW());
+        $convertedAmount = $bank2->convert($krwAmount, Currency::USD());
+
+        // Assert
+        $this->assertEquals(new Money(10000 / 1344 * 1.2, Currency::USD()), $convertedAmount);
+    }*/
+
+//    $this->expectExceptionMessage('EUR->KRW');
+
+
+    public function test_error_for_add_same_exchange_rate_as_pivot(){
+        // Assert
+        $this->expectExceptionMessage('An Exchange Rate can not be added for the Pivot Currency');
+
+        // Arrange
+        $this->bank->addExchangeRate(Currency::EUR(), Currency::USD(), 1.2);
+    }
+
+    public function test_error_for_add_exchange_rate_equal_0(){
+        // Assert
+        $this->expectExceptionMessage('Exchange Rate should be greater than 0');
+
+        // Arrange
+        $this->bank->addExchangeRate(Currency::EUR(), Currency::KRW(), 0);
+    }
+
+    public function test_error_for_add_exchange_rate_less_than_0(){
+        // Assert
+        $this->expectExceptionMessage('Exchange Rate should be greater than 0');
+
+        // Arrange
+        $this->bank->addExchangeRate(Currency::EUR(), Currency::KRW(), -1);
+    }
+
+    public function test_add_a_float_exchange_rate_of_1_298989888(){
+        // Act
+        $nbElementsBefore = count($this->bank->getExchangeRates());
+
+        // Arrange
+        $this->bank = $this->bank->addExchangeRate(Currency::EUR(), Currency::KRW(), 1.298989888);
+
+        // Act
+        $nbElementsAfter = count($this->bank->getExchangeRates());
+
+        // Assert
+        $this->assertEquals($nbElementsBefore+2, $nbElementsAfter);
+        $this->assertEquals(1.298989888, $this->bank->getExchangeRates()[Currency::EUR() .'->'. Currency::KRW()]);
+        $this->assertEquals(1/1.298989888, $this->bank->getExchangeRates()[Currency::KRW() .'->'. Currency::EUR()]);
+    }
+
+    /*Scenario Update an Exchange Rate
+    Given a Bank with EUR as Pivot Currency
+    And an Exchange Rate of 1.2 for Currency USD
+    When I update an Exchange Rate for the Currency USD to 1.3
+    Then the Exchange Rate from EUR to USD should be 1.3*/
+
+    public function test_update_an_exchange_rate_of_1_2_for_USD_to_1_3(){
+        // Arrange
+        $this->bank = $this->bank->addExchangeRate(Currency::EUR(), Currency::USD(), 1.3);
+
+        // Assert
+        $this->assertEquals(1.3, $this->bank->getExchangeRates()[Currency::EUR() .'->'. Currency::USD()]);
+    }
+
 }
